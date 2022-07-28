7865d2d71919d52f1f6d14081891006dd4025d89
sanjana01-p <sanjanamurthy007@gmail.com>
2022-07-28 15:59:41 +0530

    sum of n integers

---
+++ sum_of_n_integers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,sum=0;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){
+	    sum=arr[i]+sum;
+	}
+	cout<<sum;
+}
